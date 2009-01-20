68ca4d29e85f7b4e494f10c60843486f8d903836
spob <spob@a3c5bf5b-694f-0410-b062-bbea8a01288a>
2009-01-20 02:16:24 +0000

    Added column reset info
    git-svn-id: https://openmind.svn.sourceforge.net/svnroot/openmind/trunk@672 a3c5bf5b-694f-0410-b062-bbea8a01288a

--- db/migrate/20090118042341_populate_rebuild_index_periodic_job.rb
+++ db/migrate/20090118042341_populate_rebuild_index_periodic_job.rb
@@ -1,5 +1,6 @@
 class PopulateRebuildIndexPeriodicJob < ActiveRecord::Migration
   def self.up
+    RunAtPeriodicJob.reset_column_information
     # Regenerate SOLR indexes
     RunAtPeriodicJob.create(:job => 'Topic.rebuild_solr_index; TopicComment.rebuild_solr_index', :run_at_minutes => 210) # run at 3:30AM
   end
